d52f3eaf00cfba7a1ee10f9317fe846614a4ad5a
al-masud007 <almasudsakib@gmail.com>
2026-01-10 23:47:38 +0600

    Button & Cardtheme upgrade done

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,27 @@ var kColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData().copyWith(
-      colorScheme: kColorScheme,
-      appBarTheme: const AppBarTheme().copyWith(
-        backgroundColor: kColorScheme.onPrimaryContainer,
-        foregroundColor: kColorScheme.primaryContainer,
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+          backgroundColor: kColorScheme.primaryContainer,
+        )),
       ),
+      home: const Expenses(),
     ),
-    home: const Expenses(),
-  ));
+  );
 }
